c0ce61a558d02d3db12c5dc662d1f994270325d8
Ronnie_Ahmed <rksraisul@gmail.com>
2023-11-15 13:02:07 +0600

    Println

--- src/print.rs
+++ src/print.rs
@@ -1,5 +1,18 @@
 pub fn run(){
  println!("Hello Run");
  println!("Number is {}",123);
+ //Basic Formatting
+ println!(" First Name: {} \n Last Name: {}","Ronnie","Ahmed");
+
+ //Positional Formatting
+ println!(" Fist: {0} \n Second: {1} \n Fisrt: {0}","Fisrt","Second");
+
+ //Named Arguments
+ println!("{name} likes to Learn {name2} things",name="ROnnie",name2="things");
+
+ //Arithmetic Operation
+ println!("{}",10+10);
+
+
 
 }
